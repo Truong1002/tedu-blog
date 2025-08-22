e3c2cb8b44a5efd0b4712bc5ef863343f22b58b5
Truong1002 <truongphamxuan724@gmail.com>
2025-08-22 14:29:36 +0700

    2.9. Add repository and unit of work

---
+++ src/TeduBlog.Data/SeedWorks/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduBlog.Core.SeedWorks;
+
+namespace TeduBlog.Data.SeedWorks
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly TeduBlogContext _context;
+
+        public UnitOfWork(TeduBlogContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
