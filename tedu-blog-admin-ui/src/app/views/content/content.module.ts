a214d3dd13c6c829412c16384d655661023b1c32
Truong1002 <truongphamxuan724@gmail.com>
2025-09-22 15:08:34 +0700

    3.2. Add routing for Angular

--- tedu-blog-admin-ui/src/app/views/content/content.module.ts
+++ tedu-blog-admin-ui/src/app/views/content/content.module.ts
@@ -12,17 +12,15 @@ import {
   NavModule,
   ProgressModule,
   TableModule,
-  TabsModule
+  TabsModule,
 } from '@coreui/angular';
 import { IconModule } from '@coreui/icons-angular';
 import { ChartjsModule } from '@coreui/angular-chartjs';
-
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard.component';
+import { ContentRoutingModule } from './content-routing.module';
 
 @NgModule({
   imports: [
-    DashboardRoutingModule,
+    ContentRoutingModule,
     CardModule,
     NavModule,
     IconModule,
@@ -39,7 +37,6 @@ import { DashboardComponent } from './dashboard.component';
     AvatarModule,
     TableModule,
   ],
-  declarations: [DashboardComponent]
+  declarations: [],
 })
-export class DashboardModule {
-}
+export class ContentModule {}
