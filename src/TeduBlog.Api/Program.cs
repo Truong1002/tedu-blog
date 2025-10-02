ed2f5596770f017cd0c0a94fa3ce846f4945e016
Truong1002 <truongphamxuan724@gmail.com>
2025-10-02 19:25:36 +0700

    3.5 Get permissions

--- src/TeduBlog.Api/Program.cs
+++ src/TeduBlog.Api/Program.cs
@@ -8,6 +8,7 @@ using System.Net.NetworkInformation;
 using System.Reflection;
 using System.Text;
 using TeduBlog.Api;
+using TeduBlog.Api.Filters;
 using TeduBlog.Api.Services;
 using TeduBlog.Core.ConfigOptions;
 using TeduBlog.Core.Domain.Identity;
