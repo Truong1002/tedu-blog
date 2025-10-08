d3d81a77cc9ceb89ed671a3a071996cf26a03064
Truong1002 <truongphamxuan724@gmail.com>
2025-10-08 10:45:08 +0700

    3.7. Login with Angular

--- src/TeduBlog.Api/Program.cs
+++ src/TeduBlog.Api/Program.cs
@@ -21,7 +21,16 @@ using TeduBlog.Data.SeedWorks;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 var connectionString = configuration.GetConnectionString("DefaultConnection");
-// Add services to the container.
+
+var TeduCorsPolicy = "TeduCorsPolicy";
+
+builder.Services.AddCors(o => o.AddPolicy(TeduCorsPolicy, builder =>
+{
+    builder.AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins(configuration["AllowedOrigins"])
+        .AllowCredentials();
+}));
 
 //Config DB Context and ASP.NET Core Identity
 builder.Services.AddDbContext<TeduBlogContext>(options =>
@@ -140,6 +149,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+app.UseCors(TeduCorsPolicy);
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
