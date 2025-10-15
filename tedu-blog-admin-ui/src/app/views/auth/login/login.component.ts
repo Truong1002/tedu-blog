8c5e761677bdacca2e989041540acd058fd711d1
Truong1002 <truongphamxuan724@gmail.com>
2025-10-15 23:38:32 +0700

    3.9. Logout and loading

--- tedu-blog-admin-ui/src/app/views/auth/login/login.component.ts
+++ tedu-blog-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -14,13 +14,16 @@ import {
 import { AlertService } from '../../../shared/services/alert.service';
 import { UrlConstants } from '../../../shared/constants/url.constants';
 import { TokenStorageService } from '../../../shared/services/token-storage.service';
+import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy{
   loginForm: FormGroup;
+  private ngUnsubscribe = new Subject<void>();
+  loading = false;
   constructor(
     private fb: FormBuilder,
     private authApiClient: AdminApiAuthApiClient,
@@ -34,17 +37,25 @@ export class LoginComponent {
     });
   }
 
+   ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
   login() {
+    this.loading = true;
     var request: LoginRequest = new LoginRequest({
       userName: this.loginForm.controls['userName'].value,
       password: this.loginForm.controls['password'].value,
     });
 
-    this.authApiClient.login(request).subscribe({
+     this.authApiClient.login(request)
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe({
       next: (res: AuthenticatedResult) => {
         //Save token and refresh token to localstorage
-        this.tokenSerivce.saveToken(res.token);
-        this.tokenSerivce.saveRefreshToken(res.refreshToken);
+        this.tokenSerivce.saveToken(res.token!);
+        this.tokenSerivce.saveRefreshToken(res.refreshToken!);
         this.tokenSerivce.saveUser(res);
         //Redirect to dashboard
         this.router.navigate([UrlConstants.HOME]);
@@ -52,6 +63,7 @@ export class LoginComponent {
       error: (error: any) => {
         console.log(error);
         this.alertService.showError('Đăng nhập không đúng');
+        this.loading = false;
       },
     });
   }
